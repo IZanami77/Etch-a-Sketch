937dd4d67a239ab32a961366e0af93eb807b4533
IZanami77 <iZanamiNoOkami7@gmail.com>
2024-07-02 19:05:31 -0600

    Created the basic structure for the project

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
+
+const btn = document.querySelector("button");
+
+
+
+
+
+
+// for(i = 0; i < 16*16; i++){
+// const div = document.createElement("div");
+// div.style.border = "solid white 1px";
+// container.appendChild(div);
+// }
+
+// btn.addEventListener('click',() =>{
+
+//     container.innerHTML = ""
+//     squares = prompt("How many squares do you need now? ");
+
+//     if(squares < 100){
+//     container.innerHTML = ""
+//     for(i = 0; i < squares*squares; i++){
+//         const div = document.createElement("div");
+//         container.appendChild(div);
+//     }
+//     }
+//     else{
+//         alert("The number of squares have to be inferior of 100");
+//     }
+
+
+// });
