0c6e97e43e646d99dc151523ba8948338b3e07da
IZanami77 <iZanamiNoOkami7@gmail.com>
2024-07-03 12:41:07 -0600

    feature Added in the container the squares and the button to adjust the size and with javascript resize

--- script.js
+++ script.js
@@ -4,31 +4,22 @@ const body = document.querySelector("body");
 const btn = document.querySelector("button");
 
 
+for(i = 0; i<16 *16;i++){
+    const div = document.createElement("div");
+    div.style.border = "solid 1px white";
+    container.appendChild(div);
+}
 
-
-
-
-// for(i = 0; i < 16*16; i++){
-// const div = document.createElement("div");
-// div.style.border = "solid white 1px";
-// container.appendChild(div);
-// }
-
-// btn.addEventListener('click',() =>{
-
-//     container.innerHTML = ""
-//     squares = prompt("How many squares do you need now? ");
-
-//     if(squares < 100){
-//     container.innerHTML = ""
-//     for(i = 0; i < squares*squares; i++){
-//         const div = document.createElement("div");
-//         container.appendChild(div);
-//     }
-//     }
-//     else{
-//         alert("The number of squares have to be inferior of 100");
-//     }
-
-
-// });
+btn.addEventListener('click',() =>{
+    stored = prompt("How many squares do you need?");
+    container.innerHTML ='';
+    for(i = 0; i < stored * stored;i++){
+        const divs = document.createElement("div");
+        const width = (1/stored)*100;
+        const height = (1/stored)*100;
+        divs.style.border = "solid 1px white";
+        divs.style.width = width+"%";
+        divs.style.height = height+"%";
+        container.appendChild(divs);
+    } 
+});
