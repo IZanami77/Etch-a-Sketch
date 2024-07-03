0cb4467284f120c59a29f88625e3b6056732c51b
IZanami77 <iZanamiNoOkami7@gmail.com>
2024-07-03 16:41:42 -0600

    Now able to draw into the sketch

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ for(i = 0; i<16 *16;i++){
     const div = document.createElement("div");
     div.style.border = "solid 1px white";
     container.appendChild(div);
+
+    div.addEventListener('mouseover',()=>{
+        div.style.backgroundColor = "red";
+    })
 }
 
 btn.addEventListener('click',() =>{
     stored = prompt("How many squares do you need?");
+
+    if(stored > 100) return alert("The max number is 99");
     container.innerHTML ='';
     for(i = 0; i < stored * stored;i++){
         const divs = document.createElement("div");
@@ -21,5 +27,14 @@ btn.addEventListener('click',() =>{
         divs.style.width = width+"%";
         divs.style.height = height+"%";
         container.appendChild(divs);
+        
+        divs.addEventListener('mouseover',()=>{
+            divs.style.backgroundColor = "red";
+        });
+        
     } 
 });
+
+
+
+
